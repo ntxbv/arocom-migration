53d7cf9221308dab22be5afa391ff5e5e6d12f4c
ntxbv <80267400+ntxbv@users.noreply.github.com>
2021-07-14 12:34:26 +0200

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -59,31 +59,31 @@ class Plugin implements PluginInterface, EventSubscriberInterface{
     /**
      * {@inheritdoc}
      */
-public function applyaroscaffs(Event $event): void {
-    $fs = new Filesystem();
-    $drupalFinder = new DrupalFinder();
-    $drupalFinder->locateRoot(getcwd());
-    $drupalRoot = $drupalFinder->getDrupalRoot();
-    $composerRoot = $drupalFinder->getComposerRoot();
-    if (!$fs->exists($composerRoot . '.ahoy.yml') || ($fs->exists($composerRoot . '.ahoy.yml'))) {
-        copy($composerRoot . '/vendor/arocom-migration/.ahoy.yml', $composerRoot . '/.ahoy.yml');
-    }
-    // Make sure that settings.docker.php gets called from settings.php.
-    $settingsPath = $composerRoot . '/settings/default';
-    $settingsPhpFile = $settingsPath . '/settings.php';
-    if ($fs->exists($settingsPhpFile)) {
-        $settingsPhp = file_get_contents($settingsPhpFile);
-        if (strpos($settingsPhp, 'settings.docker.php') === FALSE) {
-            $settingsPhp .= "\n\nif (file_exists('../drupal/sites/default/settings.docker.php')) {\n  include  '../drupal/sites/default/settings.docker.php';\n}\n";
-            file_put_contents($settingsPhpFile, $settingsPhp);
+    public function applyaroscaffs(Event $event): void {
+        $fs = new Filesystem();
+        $drupalFinder = new DrupalFinder();
+        $drupalFinder->locateRoot(getcwd());
+        $drupalRoot = $drupalFinder->getDrupalRoot();
+        $composerRoot = $drupalFinder->getComposerRoot();
+        if (!$fs->exists($composerRoot . '.ahoy.yml') || ($fs->exists($composerRoot . '.ahoy.yml'))) {
+            copy($composerRoot . '/vendor/arocom/arocom-migration/.ahoy.yml', $composerRoot . '/.ahoy.yml');
         }
-    }
-    // Append the settings files from the project onto l3d settings files
+        // Make sure that settings.docker.php gets called from settings.php.
+        $settingsPath = $composerRoot . '/settings/default';
+        $settingsPhpFile = $settingsPath . '/settings.php';
+        if ($fs->exists($settingsPhpFile)) {
+            $settingsPhp = file_get_contents($settingsPhpFile);
+            if (strpos($settingsPhp, 'settings.docker.php') === FALSE) {
+                $settingsPhp .= "\n\nif (file_exists('../drupal/sites/default/settings.docker.php')) {\n  include  '../drupal/sites/default/settings.docker.php';\n}\n";
+                file_put_contents($settingsPhpFile, $settingsPhp);
+            }
+        }
+        // Append the settings files from the project onto l3d settings files
         exec("sed -i -e '/<?php/{r ./drupal/sites/default/default.settings.php' -e 'd}' ./settings/default/settings.php");
         exec("sed -i -e '/<?php/{r ./drupal/sites/example.settings.local.php' -e 'd}' ./settings/default/settings.local.php");
         exec("sed -i -e '168d' ./settings/default/settings.local.php");
 
-}
+    }
 
 
 
