7fb257bfa11e775d2083b5f45496d12897d393a9
ntxbv <80267400+ntxbv@users.noreply.github.com>
2021-07-23 16:24:10 +0200

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -134,6 +134,11 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $handlerPhpFile = $handlerPath . '/ScriptHandler.php';
         file_put_contents($handlerPhpFile, str_replace('file_put_contents("$drupalRoot/sites/development.services.yml", $yaml);', 'file_put_contents("$drupalRoot/sites/default/development.services.yml", $yaml);', file_get_contents($handlerPhpFile)));
         file_put_contents($handlerPhpFile, str_replace('redis-cli flushall 2>&1', 'ahoy ar re 2>&1', file_get_contents($handlerPhpFile)));
+        // Adding Files to .gitignores
+        $gitIgnore = $composerRoot . '/.gitignore';
+        if (strpos($gitIgnore, 'settings/') === FALSE) {
+            "echo settings/ >> .gitignore";
+        }
         }
 
 
